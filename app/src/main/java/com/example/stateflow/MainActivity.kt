90c1887840c920b01718ceed0c969e32e76b1371
abhinay <abhinay212@gmail.com>
2024-11-29 00:47:12 +0530

    updatede view model

--- app/src/main/java/com/example/stateflow/MainActivity.kt
+++ app/src/main/java/com/example/stateflow/MainActivity.kt
@@ -2,19 +2,69 @@ package com.example.stateflow
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isInvisible
+import androidx.lifecycle.lifecycleScope
+import com.example.stateflow.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            viewModel.login(
+                binding.etUsername.text.toString(),
+                binding.etPassword.text.toString()
+            )
         }
+
+        lifecycleScope.launchWhenStarted {
+
+            viewModel.loginUiState.collect{
+
+                when(it) {
+                    is MainViewModel.LoginUiState.Success -> {
+                        Snackbar.make(
+                            binding.root,
+                            "Successfully logged in",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        binding.progressBar.isInvisible = false
+
+                    }
+                    is MainViewModel.LoginUiState.Error -> {
+                        Snackbar.make(
+                            binding.root,
+                            it.message,
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        binding.progressBar.isInvisible = false
+                    }
+                    is MainViewModel.LoginUiState.Loading -> {
+                        binding.progressBar.isInvisible = true
+                    }
+                    else -> Unit
+                }
+
+            }
+        }
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
